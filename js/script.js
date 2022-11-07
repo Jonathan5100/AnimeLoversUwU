af19fefa8ba8f8e4a966f96ebac1d3a148d190c2
Jonathan5100 <jonathanrodriguez5100@gmail.com>
2022-11-07 10:40:25 -0700

    layed out html and image selector

---
+++ js/script.js
@@ -0,0 +1,21 @@
+anime_data = d3.csv("./data/crunchy.csv");
+
+Promise.all([anime_data]).then((data) => {
+    let anime_data = data[0];
+    console.log(anime_data);
+    let selector = d3.select("#anime_selector");
+    selector.selectAll('option').data(anime_data)
+    .enter()
+    .append('option')
+    .attr('value', function(d) {
+      return d.anime_img;
+    })
+    .text(function(d) {
+      return d.anime;
+    });
+    selector.on("change", function(d) {
+        var selectedOption = d3.select(this).property("value")
+        d3.select("#anime_image")
+        .attr("src", selectedOption);
+    })
+});
