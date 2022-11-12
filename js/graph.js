5a0fc57e848f15248cbeecc079afc0c55edf35fb
asingh1299 <asingh1299@gmail.com>
2022-11-11 22:40:52 -0700

    played with graph height

--- js/graph.js
+++ js/graph.js
@@ -4,7 +4,7 @@ class Graph {
 
         let graph = d3.select("#graph-div");
         const svgWidth = 2000;
-        const svgHeight = 2000;
+        const svgHeight = 1100;
         //get all genres
         let formattedGenres = Object.keys(this.globalApplicationState.anime_data[0]).filter(key => key.includes("genre")).map((genre) => {
             let formatteGenre = genre.replace("genre_", "");
@@ -55,7 +55,7 @@ class Graph {
             .enter()
             .append("line")
             .style("stroke", "#aaa")
-        // Now we create the node group, and the nodes inside it
+            // Now we create the node group, and the nodes inside it
         let nodeLayer = svg.append("g")
             .attr("class", "nodes");
         // Initialize the nodes
@@ -66,33 +66,33 @@ class Graph {
             .append("circle")
             .attr("r", 5)
             .style("fill", "#69b3a2")
-        // We can add a tooltip to each node, so when you hover over a circle, you 
-        //  see the node's id
+            // We can add a tooltip to each node, so when you hover over a circle, you 
+            //  see the node's id
         nodes.append("title")
             .text(d => d.name);
 
 
         // Let's list the force we wanna apply on the network
-        var simulation = d3.forceSimulation(genresNodes)                 // Force algorithm is applied to data.nodes
-            .force("link", d3.forceLink()                               // This force provides links between nodes
-                .id(function (d) { return d.id; })                     // This provide  the id of a node
-                .links(animeLinks)                                    // and this the list of links
+        var simulation = d3.forceSimulation(genresNodes) // Force algorithm is applied to data.nodes
+            .force("link", d3.forceLink() // This force provides links between nodes
+                .id(function(d) { return d.id; }) // This provide  the id of a node
+                .links(animeLinks) // and this the list of links
             )
-            .force("charge", d3.forceManyBody().strength(-400))         // This adds repulsion between nodes. Play with the -400 for the repulsion strength
-            .force("center", d3.forceCenter(svgWidth / 2, svgHeight / 2))     // This force attracts nodes to the center of the svg area
+            .force("charge", d3.forceManyBody().strength(-400)) // This adds repulsion between nodes. Play with the -400 for the repulsion strength
+            .force("center", d3.forceCenter(svgWidth / 2, svgHeight / 2)) // This force attracts nodes to the center of the svg area
             .on("end", ticked);
 
         // This function is run at each iteration of the force algorithm, updating the nodes position.
         function ticked() {
             links
-                .attr("x1", function (d) { return d.source.x; })
-                .attr("y1", function (d) { return d.source.y; })
-                .attr("x2", function (d) { return d.target.x; })
-                .attr("y2", function (d) { return d.target.y; });
+                .attr("x1", function(d) { return d.source.x; })
+                .attr("y1", function(d) { return d.source.y; })
+                .attr("x2", function(d) { return d.target.x; })
+                .attr("y2", function(d) { return d.target.y; });
 
             nodes
-                .attr("cx", function (d) { return d.x + 6; })
-                .attr("cy", function (d) { return d.y - 6; });
+                .attr("cx", function(d) { return d.x + 6; })
+                .attr("cy", function(d) { return d.y - 6; });
         }
 
     }
