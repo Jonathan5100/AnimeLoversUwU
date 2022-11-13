9c7ae931a07c765ab54205ef7a62fc532db4b359
Jonathan5100 <jonathanrodriguez5100@gmail.com>
2022-11-12 18:54:45 -0700

    added navbar

--- js/pies.js
+++ js/pies.js
@@ -1,5 +1,5 @@
 class Pies {
-    radius = 450;
+    radius = 300;
     constructor(globalApplicationState){
         this.globalApplicationState = globalApplicationState;
         this.svg_anime =  d3.select('#pie-chart-anime-div').append('svg')
@@ -19,22 +19,14 @@ class Pies {
 
         this.drawPie(this.svg_anime, data_anime);
         this.drawPie(this.svg_genre, data_genre);
-        // console.log("all in genre " + this.selected_genre + " are ")
-        // console.log(this.globalApplicationState.anime_utils.getAllInGenres(this.globalApplicationState.anime_data, this.selected_genre));
-        // var ordScale = d3.scaleOrdinal()
-        //                     .domain()
-        //                     .range(['#ffd384','#94ebcd','#fbaccc','#d3e0ea','#fa7f72']);
-        // d3.select("#pie-chart-anime")
-        // .join()
-        // .append('svg')
     }
     getRatingsPerAnime(anime){
         let data = [];
-        data.push({"stars": "Five Star", "rate_count": (parseInt(anime.rate_5) / parseInt (anime.votes) ) * 100});
-        data.push({"stars": "Four Star", "rate_count": (parseInt(anime.rate_4) / parseInt (anime.votes) ) * 100});
-        data.push({"stars": "Three Star", "rate_count": (parseInt(anime.rate_3) / parseInt (anime.votes) ) * 100} );
-        data.push({"stars": "Two Star", "rate_count": (parseInt(anime.rate_2) / parseInt (anime.votes) ) * 100});
-        data.push({"stars": "One Star", "rate_count": (parseInt(anime.rate_1) / parseInt (anime.votes) ) * 100});
+        data.push({"stars": "Five Star", "rate_count": (parseInt(anime.rate_5) / parseInt (anime.votes) ) * 100, "order" : 1});
+        data.push({"stars": "Four Star", "rate_count": (parseInt(anime.rate_4) / parseInt (anime.votes) ) * 100, "order" : 2});
+        data.push({"stars": "Three Star", "rate_count": (parseInt(anime.rate_3) / parseInt (anime.votes) ) * 100, "order" : 3} );
+        data.push({"stars": "Two Star", "rate_count": (parseInt(anime.rate_2) / parseInt (anime.votes) ) * 100, "order" : 4});
+        data.push({"stars": "One Star", "rate_count": (parseInt(anime.rate_1) / parseInt (anime.votes) ) * 100, "order" : 5});
         return data;
     }
     getRatingsPerGenre(genre){
@@ -56,11 +48,11 @@ class Pies {
             return sum + parseInt(anime.rate_1)
           }, 0);
         let votes = total5 + total4 + total3 + total2 + total1;
-        data.push({"stars": "Five Star", "rate_count": (total5 /votes ) * 100});
-        data.push({"stars": "Four Star", "rate_count": (total4 /votes ) * 100});
-        data.push({"stars": "Three Star", "rate_count": (total3 /votes ) * 100} );
-        data.push({"stars": "Two Star", "rate_count": (total2 /votes ) * 100});
-        data.push({"stars": "One Star", "rate_count": (total1 /votes ) * 100});
+        data.push({"stars": "Five Star", "rate_count": (total5 /votes ) * 100, "order" : 1});
+        data.push({"stars": "Four Star", "rate_count": (total4 /votes ) * 100, "order" : 2});
+        data.push({"stars": "Three Star", "rate_count": (total3 /votes ) * 100, "order" : 3} );
+        data.push({"stars": "Two Star", "rate_count": (total2 /votes ) * 100, "order" : 4});
+        data.push({"stars": "One Star", "rate_count": (total1 /votes ) * 100, "order" : 5});
         return data;
     }
     drawPie(svg, data){
@@ -69,7 +61,9 @@ class Pies {
         .range(['#ffd384','#94ebcd','#fbaccc','#d3e0ea','#fa7f72']);
         let pie = d3.pie().value(function(d) { 
             return d.rate_count; 
-        });
+        }).sort(function (a, b) {
+            return b.order - a.order
+          });
         let g = svg.append("g").attr("transform", "translate(" + 450 + "," + 450 + ")");
         let arc = g.selectAll("arc")
                 .data(pie(data))
@@ -81,9 +75,10 @@ class Pies {
         arc.append("path")
             .attr("d", path)
             .attr("fill", function(d) { return ordScale(d.data.stars); });
-
+            
+       
             let label = d3.arc()
-            .outerRadius(this.radius)
+            .outerRadius(this.radius + 100)
             .innerRadius(0);
   
             arc.append("text")
@@ -93,5 +88,6 @@ class Pies {
             .text(function(d) { return d.data.stars; })
             .style("font-family", "arial")
             .style("font-size", 15);
+    
     }
 }
