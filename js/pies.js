9487460f2b5bca5a334b229d9688697f290ec70f
Jonathan5100 <jonathanrodriguez5100@gmail.com>
2022-11-21 11:47:13 -0700

    tooltip beta

--- js/pies.js
+++ js/pies.js
@@ -94,7 +94,7 @@ class Pies {
         d3.select('#G1').text(Math.round(this.data_genre[4].rate_count  * 100) / 100 );
     }
     updateGenre(){
-        this. data_genre = this.getRatingsPerGenre(this.globalApplicationState.selected_genre);
+        this.data_genre = this.getRatingsPerGenre(this.globalApplicationState.selected_genre);
         d3.select("#genre_header").text('' + this.globalApplicationState.selected_genre.replaceAll('_', ' ') );
         if(this.data_anime !== null)
             this.storyTelling();
@@ -103,11 +103,11 @@ class Pies {
     }
     getRatingsPerAnime(anime){
         let data = [];
-        data.push({"stars": "Five Star", "rate_count": (parseInt(anime.rate_5) / parseInt (anime.votes) ) * 100, "order" : 1});
-        data.push({"stars": "Four Star", "rate_count": (parseInt(anime.rate_4) / parseInt (anime.votes) ) * 100, "order" : 2});
-        data.push({"stars": "Three Star", "rate_count": (parseInt(anime.rate_3) / parseInt (anime.votes) ) * 100, "order" : 3} );
-        data.push({"stars": "Two Star", "rate_count": (parseInt(anime.rate_2) / parseInt (anime.votes) ) * 100, "order" : 4});
-        data.push({"stars": "One Star", "rate_count": (parseInt(anime.rate_1) / parseInt (anime.votes) ) * 100, "order" : 5});
+        data.push({"stars": "Five Star", "rate_count": (parseInt(anime.rate_5) / parseInt (anime.votes) ) * 100, "order" : 1, "total" :anime.rate_5});
+        data.push({"stars": "Four Star", "rate_count": (parseInt(anime.rate_4) / parseInt (anime.votes) ) * 100, "order" : 2, "total" :anime.rate_4});
+        data.push({"stars": "Three Star", "rate_count": (parseInt(anime.rate_3) / parseInt (anime.votes) ) * 100, "order" : 3, "total" :anime.rate_3} );
+        data.push({"stars": "Two Star", "rate_count": (parseInt(anime.rate_2) / parseInt (anime.votes) ) * 100, "order" : 4 , "total" :anime.rate_2});
+        data.push({"stars": "One Star", "rate_count": (parseInt(anime.rate_1) / parseInt (anime.votes) ) * 100, "order" : 5, "total" :anime.rate_1});
         return data;
     }
     getRatingsPerGenre(genre){
@@ -129,11 +129,11 @@ class Pies {
             return sum + parseInt(anime.rate_1)
           }, 0);
         let votes = total5 + total4 + total3 + total2 + total1;
-        data.push({"stars": "Five Star", "rate_count": (total5 /votes ) * 100, "order" : 1});
-        data.push({"stars": "Four Star", "rate_count": (total4 /votes ) * 100, "order" : 2});
-        data.push({"stars": "Three Star", "rate_count": (total3 /votes ) * 100, "order" : 3} );
-        data.push({"stars": "Two Star", "rate_count": (total2 /votes ) * 100, "order" : 4});
-        data.push({"stars": "One Star", "rate_count": (total1 /votes ) * 100, "order" : 5});
+        data.push({"stars": "Five Star", "rate_count": (total5 /votes ) * 100, "order" : 1, "total" : total5});
+        data.push({"stars": "Four Star", "rate_count": (total4 /votes ) * 100, "order" : 2, "total" : total4});
+        data.push({"stars": "Three Star", "rate_count": (total3 /votes ) * 100, "order" : 3, "total" : total3} );
+        data.push({"stars": "Two Star", "rate_count": (total2 /votes ) * 100, "order" : 4, "total" : total2});
+        data.push({"stars": "One Star", "rate_count": (total1 /votes ) * 100, "order" : 5, "total" : total1});
         return data;
     }
     drawPie(svg, data){
@@ -190,6 +190,8 @@ class Pies {
           .selectAll('rect')
           .data(d => [d])
           .join('rect')
+          .on('mouseover', (event, d) => this.updateHoverOn(event, d))
+          .on('mouseout', (event, d) => this.updateHoverOff(event, d))
           .transition()
             .duration(1200)
           .attr("fill", d => this.ordScale(d.stars));
@@ -197,6 +199,7 @@ class Pies {
         cells
           .attr("rx", 3).attr("ry", 3)
           .attr("width", cellSize).attr("height", cellSize) ;
+          
     }
     waffleMyData(data){
         data = this.forceTotal(data);
@@ -207,7 +210,7 @@ class Pies {
             let i =0;
             let num = d.rate_count;
             for(let i = 0; i < num; i++){
-                waffle.push({x : x, y:y, stars:d.stars});
+                waffle.push({x : x, y:y, stars:d.stars, total:d.total, rate_count: d.un_rounded});
                 if (x === 9){
                     x = 0;
                     y ++;
@@ -225,7 +228,9 @@ class Pies {
         let I =0;
         // let min = {rate_count: 101};
         let total = 0;
-        for(let d of data){
+        let ret = JSON.parse(JSON.stringify(data));;
+        for(let d of ret){
+            d.un_rounded = d.rate_count;
             d.rate_count = Math.ceil(d.rate_count);
             if(max.rate_count > d.rate_count){
                 max = d;
@@ -236,7 +241,38 @@ class Pies {
             total += d.rate_count;
             I++;
         }
-        data[maxI].rate_count =( data[maxI].rate_count + 100 - total);
-        return data;
+        ret[maxI].rate_count =( ret[maxI].rate_count + 100 - total);
+        return ret;
+    }
+
+    updateHoverOn(event, data){
+        let i = (data.y * 10) + data.x;
+        let r = Math.floor(i / 10);
+        let c = i - r * 10;
+        let x = c * (this.waffleSize + this.padding.x); 
+        let y = r * (this.waffleSize + this.padding.y);
+        d3.select("#genre-tooltip")
+        .attr('style',
+            "left: " + (x + 65) + "px; top : " + (y -20 )+ "px; visibility: visible")
+        let gen = this.data_genre.find(({ stars }) => stars === data.stars);
+        
+        d3.select("#genre-tooltip-header").text('' + this.globalApplicationState.selected_genre.replaceAll('_', ' '));
+        d3.select("#genre-tooltip-top").text("Number of Votes: " + gen.total);
+        d3.select("#genre-tooltip-bottom").text("Percentage of Votes: " + gen.rate_count.toFixed(3) + "%");
+        let ani = this.data_anime.find(({ stars }) => stars === data.stars);
+        d3.select("#anime-tooltip")
+        .attr('style',
+            "left: " + (x + 65) + "px; top : " +( y -20 ) + "px; visibility: visible")
+        d3.select("#anime-tooltip-header").text('' + this.globalApplicationState.selected_anime.anime.replaceAll('_', ' '));
+        d3.select("#anime-tooltip-top").text("Number of Votes: " + ani.total);
+        d3.select("#anime-tooltip-bottom").text("Percentage of Votes: " + ani.rate_count.toFixed(3) + "%");
+    }
+    updateHoverOff(event, data){
+        d3.select("#genre-tooltip")
+        .attr('style',
+            "left: " + 0 + "px; top : " + 0 + "px; visibility: hidden")
+            d3.select("#anime-tooltip")
+            .attr('style',
+                "left: " + 0 + "px; top : " + 0+ "px; visibility: hidden")
     }
 }
